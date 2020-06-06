11d4278bbf2122b4e1f5a818c93625d108f45fab
ancherys <62359633+ancherys@users.noreply.github.com>
2020-06-06 10:36:06 +0100

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -2,11 +2,11 @@ let today = new Date();
 let hourNow = today.getHours();
 let greeting;
 
-if (hourNow > 18) {
+if (hourNow >= 18) {
   greeting = "Good evening!";
-} else if (hourNow > 12) {
+} else if (hourNow >= 12) {
   greeting = "Good afternoon!";
-} else if (hourNow > 5) {
+} else if (hourNow >= 5) {
   greeting = "Good morning!";
 } else if (hourNow > 0) {
   greeting = "Hello!";
